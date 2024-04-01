307c6c35d41477b78641388b3cbf122f666ad9ef
MariaValerianoo <149009830+MariaValerianoo@users.noreply.github.com>
2024-04-01 14:40:02 -0500

    update

--- VIDEOJUEGO/Game.cpp
+++ VIDEOJUEGO/Game.cpp
@@ -80,3 +80,31 @@ public:
     }
 };
 
+int main(){
+    int num;
+
+    cout<<"----Bienvenido a GameMaster----"<<endl;
+    cout<<"Este es un Sistema de Gestión y Desarrollo de Videojuegos en C++"<<endl;
+
+    do{
+        cout<<"Aqui puedes encontrar estas funciones: "<<endl;
+        cout<<"1)Juego Ahorcado\n ";
+        cout<<"2)Juego Triqui\n ";
+        cout<<"3)Almacenamiento de videojuegos\n ";
+        cout<<"4)Ver tus favoritos\n ";
+        cout<<"5)Salir del programa\n ";
+        cout<<"Elige tu opcion: \n ";
+        cin>>num;
+        
+        switch (num)
+        {
+        case 1:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    }while(num!=7);
+}
+
---
+++ VIDEOJUEGO/juegos.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include<string>
+#include"./Videojuego.cpp"
+class Ahorcado: public Videojuego{
+    int partidas = 0;
+    string palabraInicial;
+    string mostrarPalabra;
+    int vidas;
+
+void mostrar(); // Declaración de la función mostrar
+void ingresar(char x);
+void inicializar(); // Declaración de la función inicializar
+
+int main() {
+    char jugarNuevamente;
+    do {
+        partidas++; // Incrementa el contador de partidas al iniciar una nueva partida
+        inicializar();
+        clock_t inicio = clock(); 
+        mostrar();
+        while (vidas > 0 && mostrarPalabra != palabraInicial) {
+            char x;
+            cin >> x;
+            ingresar(x);
+            mostrar();
+        }
+        clock_t fin = clock(); 
+        double tiempoJuego = (double)(fin - inicio) / CLOCKS_PER_SEC; 
+        cout << "Tiempo de juego: " << tiempoJuego << " segundos" << endl;
+        cout << "¿Deseas jugar de nuevo? (s/n): ";
+        cin >> jugarNuevamente;
+    } while (jugarNuevamente == 's' || jugarNuevamente == 'S');
+
+    cout << "Número total de partidas jugadas: " << partidas << endl;
+
+    return 0;
+}
+
+void mostrar() {
+    cout << "Vidas: " << vidas << endl;
+    cout << mostrarPalabra << endl;
+}
+
+void inicializar() {
+    vidas = 5; 
+    palabraInicial = "Ahorcado C++"; 
+    mostrarPalabra = palabraInicial; 
+
+    // Convertir la palabra inicial a minúsculas
+    for (int i = 0; i < palabraInicial.length(); i++) { 
+        if (palabraInicial[i] >= 'A' && palabraInicial[i] <= 'Z') {
+            palabraInicial[i] += 32;
+        }
+    }
+
+    // Inicializar mostrarPalabra con guiones
+    for (int i = 0; i < palabraInicial.length(); i++) {
+        if (palabraInicial[i] >= 'a' && palabraInicial[i] <= 'z') {
+            mostrarPalabra[i] = '-';
+        }
+    }
+}
+
+void ingresar(char x) {
+    bool perderVidas = true;
+
+    for (int i = 0; i < palabraInicial.length(); i++) {
+        if (x == palabraInicial[i]) { // Utiliza el operador de comparación '=='
+            perderVidas = false;
+            mostrarPalabra[i] = x;
+        }
+    }
+    if (perderVidas) {
+        vidas--;
+    }
+}
+
+};
